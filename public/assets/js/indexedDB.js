da3ee509258fc98d650549ebd3170356eea5adf4
Hekwang Lhi <hekwang.lhi@gmail.com>
2021-06-15 05:19:58 -0400

    service-worker

--- public/assets/js/indexedDB.js
+++ public/assets/js/indexedDB.js
@@ -7,6 +7,7 @@ let txStoreName = ["BudgetStore"];
 const req= indexedDB.open(databaseName, 1);
 
 req.onupgradeneeded = function(e) {
+  console.log("indexedDC: onupgradeneeded");
     // create object store called "BudgetStore" and set autoIncrement to true
   const db = e.target.result;
     // Creates an object store with a listID keypath that can be used to query on.
@@ -18,18 +19,21 @@ req.onerror = function(e) {
 };
   
 req.onsuccess = function(e) {
+  console.log("indexedDC: req.onsuccess");
   db = e.target.result;
   if (navigator.online) checkDatabase();
 };
 
 // called index.js
 function saveRecord(r) {
+  console.log("indexedDC: saveRecord");
   const tx = db.transaction(txStoreName, "readwrite");
   const store = tx.objectStore(storeName);
   store.add(r);
 };
 
 function checkDatabase() {
+  console.log("indexedDC: checkDatabased");
   const tx = db.transaction(txStoreName, "readwrite");
   const store = tx.objectStore(storeName);
   const getAll = store.getAll();
